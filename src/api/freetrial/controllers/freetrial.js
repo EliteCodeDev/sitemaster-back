90b99e350814cfcb1bc736db9c0afb5b5af8df87
EliteCodeDev <elitecode2025dev@gmail.com>
2025-03-03 13:49:49 -0500

    first commit

---
+++ src/api/freetrial/controllers/freetrial.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * freetrial controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::freetrial.freetrial');
---
+++ src/api/freetrial/routes/freetrial.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * freetrial router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::freetrial.freetrial');
